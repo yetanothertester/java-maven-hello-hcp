e5ee227f6fb3d24441c49f68683d573aa39fdd24
1084565 <spamerella@gmx.de>
2016-02-18 16:44:45 +0100

    Update HelloWorldServlet.java

--- src/com/sap/cloud/sample/helloworld/HelloWorldServlet.java
+++ src/com/sap/cloud/sample/helloworld/HelloWorldServlet.java
@@ -16,6 +16,6 @@ public class HelloWorldServlet extends HttpServlet {
     /** {@inheritDoc} */
     @Override
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        response.getWriter().println("<p>Hello HCP!</p>");
+        response.getWriter().println("<p>Hello friend!</p>");
     }
 }
